22e364701a480fdf83a08f88f0b757a615400df2
Duong Trinh <duong.triny@gmail.com>
2024-09-04 01:58:54 +0700

    Init Auth and DB

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import { expressjwt } from 'express-jwt';
+import 'dotenv/config';
+import { getCountry, getCountries, getCountriesWithGdp, getCountriesWithFilter, getCountriesWithPagination, getCountriesWithPrivate } from './api/retrieve.js';
+import init from './init-db.js';
 const app = express();
 const port = 3000;
+const secret = process.env.SECRET;
+const timeout = Number.parseInt(process.env.TIMEOUT);
 
-import { getCountry, getCountries, getCountriesWithGdp, getCountriesWithFilter, getCountriesWithPagination, getCountriesWithPrivate } from './api/retrieve.js';
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressjwt({
+    secret: secret,
+    algorithms: ["HS256"]
+}).unless({ path: ['/', '/api/login', '/api/v1/countries', '/api/v2/countries', '/api/v3/countries', '/api/v4/countries', '/api/v5/countries', /api\/v1\/countries\/.+/] }));
 
 app.get('/api/v1/countries', getCountries);
 app.get('/api/v1/countries/:code', getCountry);
@@ -11,6 +23,35 @@ app.get('/api/v3/countries', getCountriesWithFilter);
 app.get('/api/v4/countries', getCountriesWithPagination);
 app.get('/api/v5/countries', getCountriesWithPrivate);
 
+app.post('/api/login', (req, res) => {
+    // Mock user
+    const users = [{
+        id: 1,
+        username: 'admin',
+        role: 'admin',
+        password: '1234567890'
+    },
+    {
+        id: 2,
+        username: 'staff',
+        role: 'staff',
+        password: '1234567890'
+    }
+    ];
+    const user = users.find(u => u.username === req.body.username && u.password === req.body.password);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ username: user.username }, secret, { expiresIn: timeout });
+    return res.json({ token, timeout });
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.listen(port, () => {
+    init();
     console.log(`Server is running on port ${port}`);
 });
---
+++ init-db.js
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import db from 'knex';
+const knex = db({
+    client: 'pg',
+    connection: process.env.DB_CONNECTION
+});
+async function init() {
+    await knex.schema.createTableIfNotExists('customers', (table) => {
+        table.uuid('id');
+        table.string('firstName', 100);
+        table.string('lastName');
+        table.string('middleName');
+        table.string('birthday');
+        table.string('phone');
+        table.string('email');
+        table.timestamp('createdAt', { useTz: true, precision: 3 });
+        table.timestamp('updatedAt', { useTz: true, precision: 3 });
+        table.primary(['id']);
+        table.unique(['email', 'phone']);
+    });
+
+    await knex.schema.createTableIfNotExists('addresses', (table) => {
+        table.uuid('id');
+        table.uuid('customerId');
+        table.string('streetNumber');
+        table.string('street');
+        table.string('ward');
+        table.string('district');
+        table.string('city');
+        table.string('state');
+        table.string('zip');
+        table.string('country');
+        table.timestamp('createdAt', { useTz: true, precision: 3 });
+        table.timestamp('updatedAt', { useTz: true, precision: 3 });
+        table.primary(['id']);
+        table.foreign('customerId').references('customers.id');
+    });
+}
+export default init;
